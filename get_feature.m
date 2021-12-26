63877225b668f39772936c583011d2ea6892f526
seirra blake <sophietheopossum@yandex.ru>
2021-12-26 02:29:24 +0000

    Initial commit

---
+++ get_feature.m
@@ -0,0 +1,30 @@
+% Description: prepare data based on which investigation is taking place
+%
+% Inputs: x: the feature number, im: an image, words: bag of visual words,
+% optional
+% 
+% Outputs: f: the extracted feature
+function f = get_feature(x, im, words)
+    switch x
+        case 2
+            % calculate average brightness of whole image
+            f = sum(im(1:1:end)) / (size(im, 1) * size(im, 2) * size(im, 3));
+        case 3
+            % generate gradients from the image using the prewitt method
+            [Gx, Gy] = imgradientxy(im, 'Prewitt');
+            % apply thresholds to generated magnitudes in order to detect clear
+            % edges
+            Gx = abs(Gx)>=45;
+            Gy = abs(Gy)>=45;
+            % collect decimals representing the amount of horizontal and vertical
+            % edges
+            f = [(sum(sum(Gx)) / (size(Gx, 1) * size(Gx, 2))) (sum(sum(Gy)) / (size(Gy, 1) * size(Gy, 2)))];
+        case 4
+            % extract hog features using typical settings
+            f = extractHOGFeatures(im, 'CellSize', [16 16], ...
+            'BlockSize', [floor(size(im,1)/16) floor(size(im,2)/16)], ...
+            'UseSignedOrientation', true);
+        case 5
+            f = words.encode(im);
+    end
+end
---
+++ test_feature.m
@@ -0,0 +1,69 @@
+% Description: test a feature for the investigation
+%
+% Inputs: x: the feature number, matches up to the header
+% 
+% Outputs: a: the overall accuracy
+function a = test_feature(x)
+    % re-seed the random number for reproducible results
+    rng(0);
+    % read the file's data and store it in memory
+    if x == 1
+        data = readcell('MerchData.csv');
+    else
+        data = imageDatastore('MerchData', ...
+        'IncludeSubfolders', true, ...
+        'LabelSource', 'foldernames');
+    end
+    % create a 2:3 split of test:train randomised data
+    if x == 1
+        data(1,:) = []; % remove the headers
+        data = data(randperm(size(data,1)),:);
+        testSize = round(0.4 * size(data,1));
+        testData = data(1:testSize,:);
+        trainData = data(testSize+1:end,:);
+    else
+        [imdsTrain, imdsTest] = splitEachLabel(data, 0.6, 'randomize');
+    end
+    % gather the labels
+    if x == 1
+        trainCat = categorical(trainData(:,2));
+        testCat = categorical(testData(:,2));
+    else
+        trainCat = imdsTrain.Labels;
+        testCat = imdsTest.Labels;
+    end
+    % prepare the data
+    if x == 1
+        % strip the categories
+        trainData(:,2) = [];
+        testData(:,2) = [];
+        % convert to matrices
+        trainData = cell2mat(trainData(:,:));
+        testData = cell2mat(testData(:,:));
+    else
+        % initialise arrays
+        trainData = [];
+        testData = [];
+        % one feature needs words
+        if x == 5
+            words = bagOfFeatures(imdsTrain);
+        else
+            words = 0;
+        end
+        % populate arrays, feature is extracted from greyscale image
+        while hasdata(imdsTrain)
+            trainData(end+1,:) = get_feature(x, im2gray(imdsTrain.read()), words);
+        end
+        while hasdata(imdsTest)
+            testData(end+1,:) = get_feature(x, im2gray(imdsTest.read()), words);
+        end
+    end
+    % train the model
+    model = fitcknn(trainData,trainCat,"NumNeighbors",3);
+    % use the model to predict what label each row of data will have
+    predictions = predict(model, testData);
+    % create a confusion matrix to aid in performance evaluation
+    [results,~] = confusionmat(testCat, predictions);
+    % create a percentage accuracy to allow easy comparison
+    a = 100 * (sum(diag(results)) / sum(results(:)));
+end
