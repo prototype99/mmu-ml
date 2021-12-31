a43a3ac7aa4b438b289a6959a6f5c9a3d21a2ef3
seirra blake <sophietheopossum@yandex.ru>
2021-12-31 14:34:32 +0000

    add a distance calculation function

---
+++ knn_calculate_distance.m
@@ -0,0 +1,21 @@
+% Description: calculate the Euclidean distance between any two
+% points
+%
+% Inputs:
+% p: an array containing the coordinates of the first point
+% q: an array containing the coordinates of the second point
+% 
+% Outputs:
+% d: a numeric value holding the straight-line distance
+% between the two points
+% 
+% Notes: you can find the equation for Euclidean distance in the
+% lecture slides.
+function d = knn_calculate_distance(p, q)
+    % subtract the feature values in p from the corresponding feature
+    % values in q
+    d =  p - q;
+    % square each of the resulting differences and sum them then compute
+    % the square root
+    d = sqrt(sum(d * d));
+end
