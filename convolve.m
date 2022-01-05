c79c87f8a83fae4296cc27b08e0a214ca5ce22d1
seirra blake <sophietheopossum@yandex.ru>
2022-01-05 16:26:37 +0000

    add iterators

--- convolve.m
+++ convolve.m
@@ -5,9 +5,15 @@
 % 
 % Outputs: c: a convoluted image
 function c = convolve(im,f)
-    for x = im
-        for y = im
+    % make an iterator, we skip the first row, it's all padding
+    i = 2;
+    for y = im
+        % make an iterator, we skip the first column, it's all padding
+        j = 2;
+        for x = im
+            j = j + 1;
         end
+        i = i + 1;
     end
     c = f;
 end
