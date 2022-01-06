b283d7081d31af5816ce9cb901330faf61d38cdc
seirra blake <sophietheopossum@yandex.ru>
2022-01-06 17:12:18 +0000

    loop through each individual pixel

--- convolve.m
+++ convolve.m
@@ -7,13 +7,14 @@
 function c = convolve(im,f)
     % make an iterator, we skip the first row, it's all padding
     i = 2;
-    for y = im
+    for y = i:size(im,1)-1
         % make an iterator, we skip the first column, it's all padding
         j = 2;
-        for x = im
+        for x = j:size(im,2)-1
+            c(j-1,i-1) = im(i,j);
             j = j + 1;
         end
         i = i + 1;
     end
-    c = f;
+    c = c;
 end
