98514f1371714032d74955f90b420748ab18feb3
seirra blake <sophietheopossum@yandex.ru>
2022-01-06 17:53:59 +0000

    add even more for loops

--- convolve.m
+++ convolve.m
@@ -11,6 +11,18 @@ function c = convolve(im,f)
         % make an iterator, we skip the first column, it's all padding
         j = 2;
         for x = j:size(im,2)-1
+            % Xs in Xs and Ys in Ys, read one single pixel and soon you'll
+            % read nine
+            % the relative matrices exist to allow reading of all local
+            % pixels
+            xrel = [-1,-1,-1;0,0,0;1,1,1];
+            yrel = [1,0,-1;1,0,-1;1,0,-1];
+            % csum contains all filtered pixels
+            csum = 0;
+            for iy = 1:size(im,1)-1
+                for ix = 1:size(im,2)-1
+                end
+            end
             c(j-1,i-1) = im(i,j);
             j = j + 1;
         end
